1e78de044cc237404874fa586fc7a33119f9b56e
tushar <tushar.paudel884@gmail.com>
2026-06-15 15:30:50 +0530

    follow REST rules

--- src/main/java/com/firstclub/membershipprogram/controllers/SubscriptionController.java
+++ src/main/java/com/firstclub/membershipprogram/controllers/SubscriptionController.java
@@ -4,7 +4,10 @@ import com.firstclub.membershipprogram.dtos.SubscriptionRequest;
 import com.firstclub.membershipprogram.dtos.UserSubscriptionDto;
 import com.firstclub.membershipprogram.services.MembershipService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/v1/apis/subscriptions")
@@ -14,22 +17,28 @@ public class SubscriptionController {
     private MembershipService membershipService;
 
     @GetMapping("/{userName}")
-    public UserSubscriptionDto getCurrentMembership(@PathVariable String userName) {
-        return membershipService.getCurrentMembership(userName);
+    public ResponseEntity<UserSubscriptionDto> getCurrentMembership(@PathVariable String userName) {
+        UserSubscriptionDto data = membershipService.getCurrentMembership(userName);
+        return ResponseEntity.ok(data);
     }
 
     @PutMapping
-    public UserSubscriptionDto changeTier(@RequestBody SubscriptionRequest request) {
-        return membershipService.changeTier(request.getUserName(), request.getTierId());
+    public ResponseEntity<UserSubscriptionDto> changeTier(@RequestBody SubscriptionRequest request) {
+        UserSubscriptionDto data = membershipService.changeTier(request.getUserName(), request.getTierId());
+        return ResponseEntity.ok(data);
     }
 
     @PostMapping()
-    public UserSubscriptionDto createSubscription(@RequestBody SubscriptionRequest request) {
-        return membershipService.createSubscription(request);
+    public ResponseEntity<UserSubscriptionDto> createSubscription(@RequestBody SubscriptionRequest request) {
+        UserSubscriptionDto data = membershipService.createSubscription(request);
+        return ResponseEntity.ok(data);
     }
 
     @DeleteMapping("/{userName}")
-    public void updateSubscription(@PathVariable String userName) {
+    public ResponseEntity<?> cancelSubscription(@PathVariable String userName) {
         membershipService.cancelSubscription(userName);
+        return ResponseEntity.ok(Map.of(
+                "Message", "Subscription cancelled successfully"
+        ));
     }
 }
